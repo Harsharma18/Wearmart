aea5a4023f3743ce70fcb95a5aed0131169aca96
Harsharma18 <ns750759@gmail.com>
2025-04-25 23:57:42 +0530

    update product card and api

--- frontend/src/pages/Shops/Productcard.jsx
+++ frontend/src/pages/Shops/Productcard.jsx
@@ -6,9 +6,10 @@ const ProductCard = ({ products }) => {
   const dispatch = useDispatch();
   const handleAddtoCart = (item)=>{
     dispatch(addToCart(item));
-    console.log(addToCart(item));
+    // console.log(addToCart(item));
   }
- 
+
+  
   return (
     <div className="container mx-auto px-4 py-6">
       <div className="grid p-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
