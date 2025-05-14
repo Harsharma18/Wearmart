446b02c727aa0273482393e848c86530f5ef3050
Harsharma18 <ns750759@gmail.com>
2025-05-14 10:16:38 +0530

    add more field in orderschema

--- backend/src/orders/order.model.js
+++ backend/src/orders/order.model.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
-    orderId : String,
-    email:{
-        type:String,
-        required:true,
+  orderId: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  items: [
+    {
+      itemId: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
     },
-    amount : Number,
-    items:[
-        {
-            itemId : {
-                type:String,
-                required:true,
-            },
-            quantity:{
-                type:String,
-                required:true,
-            }
-        }
-    ],
-    status:{
-        type:String,
-        enum:["pending","processing","shipped","completed"],
-        default:"pending",
-    }
-})
-const Order = mongoose.model("Order",orderSchema);
+  ],
+  status: {
+    type: String,
+    enum: ["pending", "processing", "shipped", "completed"],
+    default: "pending",
+  },
+});
+const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
