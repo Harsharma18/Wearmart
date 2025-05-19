f8487b7306c5519360a76d6ae963adbc4e2452cf
Harsharma18 <ns750759@gmail.com>
2025-05-19 15:28:15 +0530

    feat(order): save order on payment success and update status field

--- backend/src/orders/order.model.js
+++ backend/src/orders/order.model.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
-const orderSchema = new mongoose.Schema({
-  orderId: String,
-  email: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  items: [
-    {
-      itemId: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
+const orderSchema = new mongoose.Schema(
+  {
+    orderId: String,
+    email: {
+      type: String,
+      required: true,
     },
-  ],
-  status: {
-    type: String,
-    enum: ["pending", "processing", "shipped", "completed"],
-    default: "pending",
+    amount: {
+      type: Number,
+      required: true,
+    },
+    items: [
+      {
+        itemId: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "completed"],
+      default: "pending",
+    },
   },
-});
+  { timestamps: true }
+);
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;
