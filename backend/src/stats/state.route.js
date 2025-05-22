8f36f2aacde504acd3c3ce1c0bb67e8c01c525f1
Harsharma18 <ns750759@gmail.com>
2025-05-22 18:19:54 +0530

    Add user and admin stats API routes

--- backend/src/stats/state.route.js
+++ backend/src/stats/state.route.js
@@ -20,84 +20,85 @@ router.get("/userstats/:email", async (req, res) => {
       { $group: { _id: null, totalamount: { $sum: "$amount" } } },
     ]);
     console.log(totalPaymentresult);
-    const totalAmountPayment = totalPaymentresult.length>0?totalPaymentresult[0].totalamount:0;
-    //get totalReviews 
-    const totalReview = await  Reviewmodel.countDocuments({author:user._id});
-      //total purchase product
-      const productPurchase = await Ordermodel.distinct("items.itemId",{email:email});
-      const totalPurchaseProducts = productPurchase.length;
-      res.json({
-         totalAmount :  totalAmountPayment.toFixed(2),
-        totalReview,
-        totalPurchaseProducts
-
-      });
-
+    const totalAmountPayment =
+      totalPaymentresult.length > 0 ? totalPaymentresult[0].totalamount : 0;
+    //get totalReviews
+    const totalReview = await Reviewmodel.countDocuments({ author: user._id });
+    //total purchase product
+    const productPurchase = await Ordermodel.distinct("items.itemId", {
+      email: email,
+    });
+    const totalPurchaseProducts = productPurchase.length;
+    res.json({
+      totalAmount: totalAmountPayment.toFixed(2),
+      totalReview,
+      totalPurchaseProducts,
+    });
   } catch (err) {
     console.error("Error fetching user stats", err);
     res.status(500).send({ message: "Failed to fetch user stats" });
   }
 });
 //admin stats route
-router.get("/adminstats",async(req,res)=>{
-    try{
-        //calculate total order 
-        const totalorders = await Ordermodel.countDocuments();
-        //calculate total product 
-        const totalProducts = await Productmodel.countDocuments();
+router.get("/adminstats", async (req, res) => {
+  try {
+    //calculate total order
+    const totalorders = await Ordermodel.countDocuments();
+    //calculate total product
+    const totalProducts = await Productmodel.countDocuments();
 
-        //calculate total review
-        const totalReview = await Reviewmodel.countDocuments();
-        //calculate total users
-        const totalusers = await Usermodel.countDocuments();
-        //calculate total earning
-        const totalEarningResult  = await Ordermodel.aggregate([
-            {
-                $group:{
-                   _id:null,
-                   totalEarning : {$sum:"$amount"},
-                }
-            }
+    //calculate total review
+    const totalReview = await Reviewmodel.countDocuments();
+    //calculate total users
+    const totalusers = await Usermodel.countDocuments();
+    //calculate total earning
+    const totalEarningResult = await Ordermodel.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalEarning: { $sum: "$amount" },
+        },
+      },
+    ]);
+    const totalEarning =
+      totalEarningResult.length > 0 ? totalEarningResult[0].totalEarning : 0;
 
-        ]);
-        const totalEarning = totalEarningResult.length>0?totalEarning[0].totalEarning:0;
-
-
-        //calculate total earning in a month
-        const totalEarningMonthlyResult = await Order.aggregate([
-            {
-                $group:{
-                    _id:{month:{$month : "$createdAt"},year:{$year:"$createdAt"}},
-                    monthlyEarning:{$sum:"$amount"}
-                }
-            },
-            {
-                //Sort by year and month
-                $sort : {
-                   "_id.year":1,
-                   "_id.month":1,
-                }
-            }
-        ]);
-        const monthlyEarnings = totalEarningMonthlyResult.map((item)=>({
-            month:item._id.month,
-            year:item._id.year,
-            earning:item.monthlyEarning,
-        }))
-        //send the aggregiate data 
-        res.status(200).json({
-            totalProducts,
-            totalReview,
-            totalusers,
-            totalorders,
-            totalEarning,
-            monthlyEarnings,
-        })
-    
-    }catch(err){
-         console.error("Error fetching admin stats:", err);
-      res.status(500).json({ message: "Failed to fetch admin stats" });
-    
-    }
-
-})
+    //calculate total earning in a month
+    const totalEarningMonthlyResult = await Order.aggregate([
+      {
+        $group: {
+          _id: {
+            month: { $month: "$createdAt" },
+            year: { $year: "$createdAt" },
+          },
+          monthlyEarning: { $sum: "$amount" },
+        },
+      },
+      {
+        //Sort by year and month
+        $sort: {
+          "_id.year": 1,
+          "_id.month": 1,
+        },
+      },
+    ]);
+    const monthlyEarnings = totalEarningMonthlyResult.map((item) => ({
+      month: item._id.month,
+      year: item._id.year,
+      earning: item.monthlyEarning,
+    }));
+    //send the aggregiate data
+    res.status(200).json({
+      totalProducts,
+      totalReview,
+      totalusers,
+      totalorders,
+      totalEarning,
+      monthlyEarnings,
+    });
+  } catch (err) {
+    console.error("Error fetching admin stats:", err);
+    res.status(500).json({ message: "Failed to fetch admin stats" });
+  }
+});
+module.exports = router;
