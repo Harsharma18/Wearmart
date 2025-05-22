8ad6e4d36035a03b8a6d1a6619ec8831dfecd6e7
Harsharma18 <ns750759@gmail.com>
2025-05-22 18:20:01 +0530

    Add order APIs: get by email/ID, update and delete by ID

--- backend/src/orders/order.route.js
+++ backend/src/orders/order.route.js
@@ -24,13 +24,13 @@ router.post("/create-checkout-session", async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`, 
+      success_url: `http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `http://localhost:5173/cancel`,
     });
 
     res.json({ id: session.id });
   } catch (error) {
-    console.error("Error creating checkout session:", error); 
+    console.error("Error creating checkout session:", error);
     res.status(500).json({ error: "Failed to create checkout session" });
   }
 });
@@ -50,7 +50,7 @@ router.post("/confirm-payment", async (req, res) => {
 
     if (!order) {
       const lineItems = session.line_items.data.map((item) => ({
-        itemId: item.price.product, 
+        itemId: item.price.product,
         quantity: item.quantity,
       }));
 
@@ -65,17 +65,107 @@ router.post("/confirm-payment", async (req, res) => {
           session.payment_intent.status === "succeeded" ? "pending" : "failed",
       });
 
-      await order.save(); 
+      await order.save();
     } else {
       order.status =
         session.payment_intent.status === "succeeded" ? "pending" : "failed";
-      await order.save(); 
+      await order.save();
     }
 
     res.json({ message: "Order processed", order });
   } catch (err) {
-    console.error("Error confirming payment:", err); 
+    console.error("Error confirming payment:", err);
     res.status(500).json({ error: "Failed to confirm payment" });
+  }
+});
+//get order by email
+router.get("/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const orders = await Order.find({ email: email });
+    if (orders.length === 0 || !orders) {
+      return res
+        .status(400)
+        .send({ orders: 0, message: "No orders found for this email" });
+    }
+    res.status(200).send({ orders });
+  } catch (err) {
+    console.error("Error fetching orders by email", err);
+    res.status(500).send({ message: "Failed to fetch orders by email" });
+  }
+});
+//get order by id
+router.get("/order/:id", async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      return res.status(404).send({ message: "Order not found" });
+    }
+    res.status(200).send(order);
+  } catch (error) {
+    console.error("Error fetching orders by user id", error);
+    res.status(500).send({ message: "Failed to fetch orders by user id" });
+  }
+});
+//get all orders
+router.get("/", async (req, res) => {
+  try {
+    const orders = await Order.find().sort({ createdAt: -1 });
+    if (orders.length === 0) {
+      return res.status(404).send({ message: "No orders found", orders: [] });
+    }
+
+    res.status(200).send(orders);
+  } catch (error) {
+    console.error("Error fetching all orders", error);
+    res.status(500).send({ message: "Failed to fetch all orders" });
+  }
+});
+// update order status
+router.put("/updateorder/:id", async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      id,
+      {
+        status: status,
+        updatedAt: new Date(),
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedOrder) {
+      return res.status(404).send({ message: "Order not found" });
+    }
+
+    res.status(200).json({
+      message: "Order status updated successfully",
+      order: updatedOrder,
+    });
+  } catch (error) {
+    console.error("Error updating order status", error);
+    res.status(500).send({ message: "Failed to update order status" });
+  }
+});
+// delete order
+router.delete("/delete-order/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedOrder = await Order.findByIdAndDelete(id);
+    if (!deletedOrder) {
+      return res.status(404).send({ message: "Order not found" });
+    }
+    res.status(200).json({
+      message: "Order deleted successfully",
+      order: deletedOrder,
+    });
+  } catch (error) {
+    console.error("Error deleting order", error);
+    res.status(500).send({ message: "Failed to delete order" });
   }
 });
 
