92620cda2fcf5218ecc7cb1475ed306be411d5ed
Harsharma18 <ns750759@gmail.com>
2025-05-25 23:01:33 +0530

    feat: add user and admin dashboard page components

---
+++ frontend/src/pages/Dashboards/DashboardLayout.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+import UserDashboardLayout from "./UserDashboardLayout";
+import AdminDashboardLayout from "./AdminDashboardLayout";
+
+function DashboardLayout() {
+  const { user } = useSelector((state) => state.auth);
+
+  const renderDashboard = () => {
+    switch (user?.role) {
+      case "user":
+        return <UserDashboardLayout />;
+      case "admin":
+        return <AdminDashboardLayout />;
+      default:
+        return <Navigate to="/login" replace />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-5 bg-gray-100">
+     
+      <aside className="col-span-1 md:col-span-1 bg-white border-r-gray-400 p-4 shadow-sm">
+        {renderDashboard()}
+      </aside>
+
+     
+      <main className="col-span-1 md:col-span-4 p-4 md:p-8">
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
---
+++ frontend/src/pages/Dashboards/UserDashboardLayout.jsx
@@ -0,0 +1,70 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutUserMutation } from "../../redux/Auth/authapi";
+import { useNavigate, Link, NavLink } from "react-router-dom";
+import { logout } from "../../redux/Auth/authSlice";
+const navItems = [
+  { path: "/dashboard", label: "Dashboard" },
+  { path: "/dashboard/orders", label: "Order" },
+  { path: "/dashboard/payments", label: "Payments" },
+  { path: "/dashboard/profile", label: "Profile" },
+  { path: "/dashboard/reviews", label: "Reviews" },
+];
+function UserDashboardLayout() {
+  const [logoutUser] = useLogoutUserMutation();
+  const {user} = useSelector((state)=>state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handlelogout = async () => {
+    try {
+      await logoutUser().unwrap();
+      dispatch(logout());
+      navigate("/");
+    } catch (error) {
+      console.error("Failed to log out", error);
+    }
+  };
+
+  return (
+    <div className="space-y-5 bg-white p-8 md:h-screen flex flex-col justify-between">
+      <div>
+        <div className="font-semibold text-2xl">
+          <Link to="/" className="font-[Poppins] text-gray-900 ">
+            Wear<span className="text-red-500">Mart.</span>
+          </Link>
+
+          <p className="text-xs  italic">User dashboard</p>
+        </div>
+
+        <hr className="mt-5 border-gray-200 shadow-2xl" />
+
+        <ul className="space-y-5 pt-5">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "text-blue-600 font-bold" : "text-black"
+                }
+                end
+                to={item.path}
+              >
+                {item.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="mb-3">
+        <hr className="mb-3 border-gray-200" />
+        <button
+          onClick={handlelogout}
+          className="text-white bg-red-500 font-medium px-5 py-1 rounded cursor-pointer"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UserDashboardLayout;
