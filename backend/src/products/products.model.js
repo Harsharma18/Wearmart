ef3b12759ebed2abda26e2e8856f32e935a7f69d
Harsharma18 <ns750759@gmail.com>
2025-06-03 17:22:38 +0530

    Update product model schema

--- backend/src/products/products.model.js
+++ backend/src/products/products.model.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
-const productsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const productsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: String,
+    description: String,
+    price: {
+      type: Number,
+      required: true,
+    },
+    oldPrice: Number,
+    image: String,
+    color: String,
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    brand: String,
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  category: String,
-  description: String,
-  price: {
-    type: Number,
-    required: true,
-  },
-  oldPrice: Number,
-  image: String,
-  color: String,
-  rating: {
-    type: Number,
-    default: 0,
-  },
-  brand: String,
-  author: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const Product = mongoose.model("Product", productsSchema);
 module.exports = Product;
