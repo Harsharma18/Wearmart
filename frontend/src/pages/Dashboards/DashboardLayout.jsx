389e58b9c91739077dfe683b108901e4f650b4b5
Harsharma18 <ns750759@gmail.com>
2025-06-05 23:15:50 +0530

    Update DashboardLayout to include new admin routes

--- frontend/src/pages/Dashboards/DashboardLayout.jsx
+++ frontend/src/pages/Dashboards/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 import UserDashboardLayout from "./UserDashboardLayout";
 import AdminDashboardLayout from "./AdminDashboardLayout";
-
+import { Toaster } from "react-hot-toast";
 function DashboardLayout() {
   const { user } = useSelector((state) => state.auth);
 
@@ -19,13 +19,14 @@ function DashboardLayout() {
   };
 
   return (
+
     <div className="min-h-screen grid grid-cols-1 md:grid-cols-5 bg-gray-100">
      
       <aside className="col-span-1 md:col-span-1 bg-white border-r-gray-400 p-4 shadow-sm">
         {renderDashboard()}
       </aside>
 
-     
+     <Toaster/>
       <main className="col-span-1 md:col-span-4 p-4 md:p-8">
         <div className="bg-white rounded-lg shadow-md p-6">
           <Outlet />
