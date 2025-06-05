c7b5e10c0d569c338688f68d47217be7d525437d
Harsharma18 <ns750759@gmail.com>
2025-06-05 23:15:35 +0530

    Update cloudinary config for secure image handling

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "Wearmart",
-    allowed_formats: ["jpg", "png", "jpeg"],
+    allowed_formats: ["jpg", "png", "jpeg","WEBP"],
   },
 });
 
